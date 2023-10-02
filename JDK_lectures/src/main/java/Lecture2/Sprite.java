7e0390e26bb7d327b10ee13b7bf59d85867ba62c
Dmitrii <datitov2009@yandex.ru>
2023-10-02 18:57:31 +0300

    initial commit

---
+++ JDK_lectures/src/main/java/Lecture2/Sprite.java
@@ -0,0 +1,24 @@
+package Lecture2;
+
+public abstract class Sprite {
+    protected float x;
+    protected float y;
+    protected float halfWidth;
+    protected float halfHeight;
+
+//    protected float getLeft () {return x - halfWidth;}
+//    protected float setLeft (float left) {x = left + halfWidth;}
+//    protected float getRight () {return x + halfWidth;}
+//    protected float setRight (float right) {x = right - halfWidth;}
+//    protected float getTop () {return y - halfHeight;}
+//    protected float setTop (float top) {y = top + halfHeight;}
+//    protected float getBottom () {return y + halfHeight;}
+//    protected float setBottom (float bottom) {y = bottom - halfHeight;}
+//
+//    protected float getWidth () {return 2f * halfWidth;}
+//    protected float getHeight () {return 2f * halfHeight;}
+
+    //void update (MainCanvas canvas, float deltaTime);
+    //void render (MainCanvas canvas, Graphics g);
+
+}
